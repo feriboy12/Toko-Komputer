fce2d043f3ef3d23d35389be1c1541e097471d48
feriboy12 <abidinar1323@gmail.com>
2023-12-04 13:45:41 +0700

    toko-komputer

---
+++ Komputer/js/script.js
@@ -0,0 +1,16 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#burger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Klick diluar side untuk menutup side bar
+
+const burger = document.querySelector("#burger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!burger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
